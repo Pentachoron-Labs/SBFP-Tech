239a75b6baa2ff77709648661c532f0a717e7f16
AJ Fleming <atrain99@github.com>
2013-05-18 09:40:27 -0500

    BlockThorium
    The old texture system was BETTER!
    (Because I know how to use it.)

---
+++ src/minecraft/SBFP/world/BlockThoriumOre.java
@@ -0,0 +1,14 @@
+package SBFP.world;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockThoriumOre extends Block{
+
+	public BlockThoriumOre(int par1) {
+		super(par1, Material.rock);
+	}
+	
+	
+	//And texturing this block is confusing...
+}
