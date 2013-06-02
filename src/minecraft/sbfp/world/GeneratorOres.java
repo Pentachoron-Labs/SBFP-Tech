2a53ce30681bcba61dd72f99a30fc25087ad8e45
atrain99 <atrain99@mchsi.com>
2013-06-02 12:19:21 -0500

    Change Generator Level to y = 6 to 40

--- src/minecraft/sbfp/world/GeneratorOres.java
+++ src/minecraft/sbfp/world/GeneratorOres.java
@@ -23,7 +23,7 @@ public final class GeneratorOres implements IWorldGenerator{
 		if(cg instanceof ChunkProviderGenerate){
 			for(int x = cx; x<cx+16; x++){
 				for(int z = cz; z<cz+16; z++){
-					for(int y = 0; y<40; y++){
+					for(int y = 6; y<40; y++){
 						if(w.getBlockId(x,y,z)==Block.stone.blockID){
 							if(w.getBlockId(x,y+1,z)==Block.waterStill.blockID&&r.nextInt(32)==0){
 								w.setBlock(x,y,z,modsbfp.blockOre.blockID,0,2); //Monazite—pools
