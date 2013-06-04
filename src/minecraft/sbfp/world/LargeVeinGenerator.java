92d7f1314e682983c555165f0a700860ad164d7d
ObsequiousNewt <obsequiousnewt@gmail.com>
2013-06-04 12:22:19 -0500

    I haz all dem orez!

--- src/minecraft/sbfp/world/LargeVeinGenerator.java
+++ src/minecraft/sbfp/world/LargeVeinGenerator.java
@@ -16,8 +16,13 @@ class LargeVeinGenerator{
 	private final double veinSize;
 	private int X, Y, Z;
 	private ArrayList<Integer[]> blocks, blocksNew;
+	private final boolean dirtFlag;
 
 	LargeVeinGenerator(int seedX, int seedY, int seedZ, World world, Random rand, int meta, double veinSize){
+		this(seedX,seedY,seedZ,world,rand,meta,veinSize,false);
+	}
+
+	LargeVeinGenerator(int seedX, int seedY, int seedZ, World world, Random rand, int meta, double veinSize, boolean dirtFlag){
 		this.seedX = seedX;
 		this.seedY = seedY;
 		this.seedZ = seedZ;
@@ -25,10 +30,11 @@ class LargeVeinGenerator{
 		this.rand = rand;
 		this.meta = meta;
 		this.veinSize = veinSize;
+		this.dirtFlag = dirtFlag;
 	}
 
 	void generate(){
-		world.setBlock(seedX,seedY,seedZ,modsbfp.blockOre.blockID,1,2);
+		world.setBlock(seedX,seedY,seedZ,modsbfp.blockOre.blockID,this.meta,2);
 		int iter = 0;
 		blocks = new ArrayList<Integer[]>();
 		blocksNew = new ArrayList<Integer[]>();
@@ -49,7 +55,7 @@ class LargeVeinGenerator{
 			blocks = blocksNew;
 			blocksNew = new ArrayList<Integer[]>();
 			if(blocks.size()==0){
-				//				FMLLog.finer("I haz a LV deposit at (%d,%d,%d) with %d blocks",seedX,seedY,seedZ,iter);
+				//				if(this.meta == 5) FMLLog.finer("I haz a %s deposit at (%d,%d,%d) with %d blocks",modsbfp.blockOre.names[this.meta],seedX,seedY,seedZ,iter);
 				break;
 			}
 		}
@@ -58,7 +64,7 @@ class LargeVeinGenerator{
 	private void setBlock(int x, int y, int z){
 		int X = this.X+x, Y = this.Y+y, Z = this.Z+z;
 		int dx = X-seedX, dy = Y-seedY, dz = Z-seedZ;
-		if(world.getBlockId(X,Y,Z)==Block.stone.blockID&&rand.nextDouble()<Math.pow(Math.pow(dx,16)+Math.pow(dy,16)+Math.pow(dz,16)+1,-this.veinSize)){
+		if((world.getBlockId(X,Y,Z)==Block.stone.blockID||this.dirtFlag&&world.getBlockId(X,Y,Z)==Block.dirt.blockID)&&rand.nextDouble()<Math.pow(Math.pow(dx,16)+Math.pow(dy,16)+Math.pow(dz,16)+1,-this.veinSize)){
 			world.setBlock(X,Y,Z,modsbfp.blockOre.blockID,meta,2);
 			blocksNew.add(new Integer[]{X,Y,Z});
 		}
