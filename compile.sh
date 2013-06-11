43385e8fc48b6cb9769a79ea7b3a1e81b1d26f55
ObsequiousNewt <obsequiousnewt@gmail.com>
2013-06-11 13:40:37 -0500

    ^_^

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sh ./recompile.sh
-sh ./reobfuscate_srg.sh
+sh ./reobfuscate.sh
 cp -urv reobf/minecraft/sbfp .
 cp -urv src/minecraft/mods .
 jar cf sbfptech-$1.$(git rev-list HEAD --count).jar sbfp mods mcmod.info
