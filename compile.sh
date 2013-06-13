1b1d8286397ccb8e907c0e6d48fd27bc6da13e8b
ObsequiousNewt <obsequiousnewt@gmail.com>
2013-06-13 10:36:11 -0500

    Fixed up mcmod.info (will now work in Eclipse, so yay.)

--- compile.sh
+++ compile.sh
@@ -3,4 +3,7 @@ sh ./recompile.sh
 sh ./reobfuscate.sh
 cp -urv reobf/minecraft/sbfp .
 cp -urv src/minecraft/mods .
+cp -uv src/mcmod.info .
 jar cf sbfptech-$1.$(git rev-list HEAD --count).jar sbfp mods mcmod.info
+#Clean up
+rm -rv sbfp mods mcmod.info
